26034aa9aaa925284396281c20e4cdfe28a5edf4
7zYBN <frazo4ka@mail.ru>
2019-03-24 23:26:05 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let loveTrianglesCount = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let firstLoverPosition = i + 1;
+    let secondLoverPosition, thirdLoverPosition;
+    for (let j = i + 1; j < preferences.length; j++) {
+      if (firstLoverPosition == preferences[j]) {
+        secondLoverPosition = j + 1;
+        for (let k = i + 1; k < preferences.length; k++) {
+          if (secondLoverPosition == preferences[k]) {
+            thirdLoverPosition = k + 1;
+            if (thirdLoverPosition == preferences[i]) {
+              loveTrianglesCount ++;
+            }
+          }
+        }
+      }
+    }
+  }
+  return loveTrianglesCount;
 };
